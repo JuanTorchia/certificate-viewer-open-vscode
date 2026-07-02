99ed207c821c66f3b9bf7239f109dc5ecbc6ed6a
Juan Torchia <54363948+JuanTorchia@users.noreply.github.com>
2026-07-02 11:33:28 -0300

    chore: migrate eslint to flat config (#28)

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+const js = require("@eslint/js");
+const tsPlugin = require("@typescript-eslint/eslint-plugin");
+
+module.exports = [
+  {
+    ignores: ["out/**", "node_modules/**", ".vscode-test/**"],
+  },
+  js.configs.recommended,
+  ...tsPlugin.configs["flat/recommended"],
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module",
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/explicit-function-return-type": "warn",
+      "no-unused-vars": "off",
+      "preserve-caught-error": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  },
+];
